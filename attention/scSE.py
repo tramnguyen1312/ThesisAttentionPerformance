e4e6525c66c72a918c5040725e8f19e87ed024d8
Bui Minh Phung <phungbminh@gmail.com>
2025-03-22 14:53:25 +0700

    first commit

---
+++ attention/scSE.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ChannelSELayer(nn.Module):
+    """
+    Re-implementation of Squeeze-and-Excitation (SE) block described in:
+        *Hu et al., Squeeze-and-Excitation Networks, arXiv:1709.01507*
+    """
+
+    def __init__(self, channels, reduction_ratio=2):
+        """
+        :param channels: No of input channels
+        :param reduction_ratio: By how much should the channels should be reduced
+        """
+        super(ChannelSELayer, self).__init__()
+        channels_reduced = channels // reduction_ratio
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.Linear(channels, channels_reduced, bias=True)
+        self.fc2 = nn.Linear(channels_reduced, channels, bias=True)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: X, shape = (batch_size, channels, H, W)
+        :return: output tensor
+        """
+        batch_size, channels, H, W = input_tensor.size()
+        # Average along each channel
+        squeeze_tensor = input_tensor.view(batch_size, channels, -1).mean(dim=2)
+
+        # channel excitation
+        fc_out_1 = self.relu(self.fc1(squeeze_tensor))
+        fc_out_2 = self.sigmoid(self.fc2(fc_out_1))
+
+        a, b = squeeze_tensor.size()
+        output_tensor = torch.mul(input_tensor, fc_out_2.view(a, b, 1, 1))
+        return output_tensor
+
+
+class SpatialSELayer(nn.Module):
+    """
+    Re-implementation of SE block -- squeezing spatially and exciting channel-wise described in:
+        *Roy et al., Concurrent Spatial and Channel Squeeze & Excitation in Fully Convolutional Networks, MICCAI 2018*
+    """
+
+    def __init__(self, channels):
+        """
+        :param channels: No of input channels
+        """
+        super(SpatialSELayer, self).__init__()
+        self.conv = nn.Conv2d(channels, 1, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor, weights=None):
+        """
+        :param weights: weights for few shot learning
+        :param input_tensor: X, shape = (batch_size, channels, H, W)
+        :return: output_tensor
+        """
+        # spatial squeeze
+        batch_size, channel, a, b = input_tensor.size()
+
+        if weights is not None:
+            weights = torch.mean(weights, dim=0)
+            weights = weights.view(1, channel, 1, 1)
+            out = F.conv2d(input_tensor, weights)
+        else:
+            out = self.conv(input_tensor)
+        squeeze_tensor = self.sigmoid(out)
+
+        # spatial excitation
+        squeeze_tensor = squeeze_tensor.view(batch_size, 1, a, b)
+        output_tensor = torch.mul(input_tensor, squeeze_tensor)
+        return output_tensor
+
+
+class ChannelSpatialSELayer(nn.Module):
+    """
+    Re-implementation of concurrent spatial and channel squeeze & excitation:
+        *Roy et al., Concurrent Spatial and Channel Squeeze & Excitation in Fully Convolutional Networks, MICCAI 2018, arXiv:1803.02579*
+    """
+
+    def __init__(self, channels, reduction_ratio=2):
+        """
+        :param channels: No of input channels
+        :param reduction_ratio: By how much should the channels should be reduced
+        """
+        super(ChannelSpatialSELayer, self).__init__()
+        self.cSE = ChannelSELayer(channels, reduction_ratio)
+        self.sSE = SpatialSELayer(channels)
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: X, shape = (batch_size, channels, H, W)
+        :return: output_tensor
+        """
+        #output_tensor = torch.max(self.cSE(input_tensor), self.sSE(input_tensor))
+        output_tensor = self.cSE(input_tensor) + self.sSE(input_tensor)
+        return output_tensor
+
+if __name__ == '__main__':
+    input=torch.randn(50,512,7,7)
+    scse = ChannelSpatialSELayer(channels=512, reduction_ratio=2)
+    output = scse(input)
+    print(output)
