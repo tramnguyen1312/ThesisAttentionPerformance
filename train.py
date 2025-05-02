a0f70a05bad0804d02ea837919b16cd8dd25a1ad
tramnguyen1312 <tramhoctap1312@gmail.com>
2025-05-02 16:56:13 +0700

    add LambdaLR

--- train.py
+++ train.py
@@ -120,7 +120,7 @@ def main():
     elif args.attention == "none":
         attention_module = None
     elif args.attention == "MHA_CBAM":
-        attention_module = HMHA_CBAM_v2(channel=512, num_heads=8, reduction=16, attention_type='CBAM')
+        attention_module = HMHA_CBAM(channel=512, num_heads=8, reduction=16)
 
      # Select backbone model
     model = None
